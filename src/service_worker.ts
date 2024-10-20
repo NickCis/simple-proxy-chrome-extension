df989e60b429eec15658192ad086bce4e02f56e0
Nicolas Cisco <ncis20@gmail.com>
2024-10-20 12:18:26 -0300

    Forcing system proxy configuration if clear didnt remove fixed servers

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -2,68 +2,84 @@ let onAuthRequiredHandler;
 
 // https://developer.chrome.com/docs/extensions/reference/api/proxy
 async function setProxy(enabled, proxy) {
-  console.log("Updating proxy configuration", enabled, proxy);
+  try {
+    console.log("Updating proxy configuration", enabled, proxy);
 
-  if (!enabled) {
-    chrome.action.setIcon({ path: "/icons/disabled-24.png" });
-    await chrome.proxy.settings.clear({});
-    return;
-  }
+    if (!enabled) {
+      chrome.action.setIcon({ path: "/icons/disabled-24.png" });
+      await chrome.proxy.settings.clear({});
 
-  chrome.action.setIcon({ path: "/icons/icon-24.png" });
-
-  const url = new URL(proxy);
-  const scheme = url.protocol.replace(":", "");
-  const port = url.port
-    ? parseInt(url.port, 10)
-    : scheme === "https"
-      ? 443
-      : scheme === "http"
-        ? 80
-        : undefined;
-
-  if (onAuthRequiredHandler)
-    chrome.webRequest.onAuthRequired.removeListener(onAuthRequiredHandler);
-
-  if (url.username && url.password && scheme.startsWith("http")) {
-    console.log("Setting onAuthRequired handler");
-
-    onAuthRequiredHandler = (details, callbackFn) => {
-      console.log("onAuthRequired", details, url.username, url.password);
-
-      if (!details.isProxy) {
-        callbackFn();
-        return;
+      // Check if proxies were actually removed, in Kiwi Browser, "clear" might not yield the correct result
+      const current = await chrome.proxy.settings.get({});
+      if (current.value.mode === "fixed_servers") {
+        await chrome.proxy.settings.set({
+          value: {
+            mode: "system",
+          },
+          scope: "regular",
+        });
       }
 
-      callbackFn({
-        authCredentials: {
-          username: url.username,
-          password: url.password,
-        },
-      });
-    };
+      return;
+    }
 
-    chrome.webRequest.onAuthRequired.addListener(
-      onAuthRequiredHandler,
-      { urls: ["<all_urls>"] },
-      ["asyncBlocking"],
-    );
-  }
+    chrome.action.setIcon({ path: "/icons/icon-24.png" });
 
-  await chrome.proxy.settings.set({
-    value: {
-      mode: "fixed_servers",
-      rules: {
-        singleProxy: {
-          scheme,
-          port,
-          host: url.hostname,
+    const url = new URL(proxy);
+    const scheme = url.protocol.replace(":", "");
+    const port = url.port
+      ? parseInt(url.port, 10)
+      : scheme === "https"
+        ? 443
+        : scheme === "http"
+          ? 80
+          : undefined;
+
+    if (onAuthRequiredHandler)
+      chrome.webRequest.onAuthRequired.removeListener(onAuthRequiredHandler);
+
+    if (url.username && url.password && scheme.startsWith("http")) {
+      console.log("Setting onAuthRequired handler");
+
+      onAuthRequiredHandler = (details, callbackFn) => {
+        console.log("onAuthRequired", details, url.username, url.password);
+
+        if (!details.isProxy) {
+          callbackFn();
+          return;
+        }
+
+        callbackFn({
+          authCredentials: {
+            username: url.username,
+            password: url.password,
+          },
+        });
+      };
+
+      chrome.webRequest.onAuthRequired.addListener(
+        onAuthRequiredHandler,
+        { urls: ["<all_urls>"] },
+        ["asyncBlocking"],
+      );
+    }
+
+    await chrome.proxy.settings.set({
+      value: {
+        mode: "fixed_servers",
+        rules: {
+          singleProxy: {
+            scheme,
+            port,
+            host: url.hostname,
+          },
         },
       },
-    },
-    scope: "regular",
-  });
+      scope: "regular",
+    });
+  } catch (error) {
+    console.error("There was some error updating proxy configuration", error);
+  }
 }
 
 async function reload() {
